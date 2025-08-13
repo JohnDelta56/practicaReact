d33887c301fbddd7abff6238cdf375a32e664944
JohnDelta56 <juancruzr.24@gmail.com>
2025-08-12 23:57:43 -0600

    Primer commit: Proyecto listo para AWS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './VillaAllende.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenid@ a mi proyecto React
         </p>
-        <a
+         <p>
+          M&oacute;dulo 10 - Pr&aacute;ctica 2.3
+        </p>
+        {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
       </header>
     </div>
   );
